c546fb8cb104863b1dd0f77fc1c69c1ddf2e3691
Sokol-shko <sokol-shkoldin@mail.ru>
2018-08-30 14:49:16 +0300

    Компановка

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>АУДИОКАР73</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+	Consectetur iste nisi soluta architecto minus magnam asperiores 
+	ducimus odit amet molestias nam dignissimos rerum quis laboriosam 
+	est alias, tenetur sint deleniti.
+</body>
+</html>
