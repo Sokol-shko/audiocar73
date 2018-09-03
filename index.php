e99e6a113c412880e944314b0fd4c39e4fc52131
Sokol-shko <sokol-shkoldin@mail.ru>
2018-09-03 14:35:27 +0300

    Наполнение структуры

--- index.php
+++ index.php
@@ -2,13 +2,52 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>АУДИОКАР73</title>
+	<link rel="stylesheet" href="libraries/normalize/normalize.css">
+	<link rel="stylesheet" href="libraries/bootstrap-3.3.7-dist/css/bootstrap.min.css">
 	<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-	Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-	Consectetur iste nisi soluta architecto minus magnam asperiores 
-	ducimus odit amet molestias nam dignissimos rerum quis laboriosam 
-	est alias, tenetur sint deleniti.
+	<div class="container">
+		<div class="top">
+			<header>
+				<div class="headerUp">
+					<div class="box1">
+						<a href="#">Вход</a>
+						|
+						<a href="#">Регистрация</a>
+					</div>
+					<div class="box2">
+						<img src="img/logo.png" alt="logo">
+						<p>AudioCar</p>
+					</div>
+					<div class="box3">
+						<a href="#">Гость</a>
+						<img src="#" alt="Корзина">
+						<p>Цена</p>
+					</div>
+				</div>
+				<nav>
+					<ul>
+						<li><a href="#">КАТАЛОГ</a></li>
+						<li><a href="#">КОНТАКТЫ</a></li>
+						<li><a href="#">НАШИ РАБОТЫ</a></li>
+						<li><a href="#">О НАС</a></li>
+						<li><a href="#">ДОСТАВКА И ОПЛАТА</a></li>
+					</ul>
+				</nav>
+			</header>
+			<main>
+				<img class="imgMain" src="img/main-theme.jpg" alt="main-theme">
+			</main>
+		</div>
+		<footer>
+			ПОДВАЛ
+		</footer>
+	</div>
+<script src="libraries/jquery-3.3.1/jquery-3.3.1.js"></script>
+<script src="libraries/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
 </body>
 </html>
