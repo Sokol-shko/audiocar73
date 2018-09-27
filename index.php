d9da1229dc9d738a308f2dcc4e1f29a2d500bae8
Sokol-shko <sokol-shkoldin@mail.ru>
2018-09-27 13:10:46 +0300

    Набросок мекетов

--- index.php
+++ index.php
@@ -8,9 +8,10 @@
 	<link rel="stylesheet" href="libraries/normalize/normalize.css">
 	<link rel="stylesheet" href="libraries/bootstrap-3.3.7-dist/css/bootstrap.min.css">
 	<link rel="stylesheet" href="css/style.css">
+	<link href="https://fonts.googleapis.com/css?family=Fira+Sans:400,700&amp;subset=cyrillic,cyrillic-ext" rel="stylesheet">
 </head>
 <body>
-	<div class="container">
+	<div class="block">
 		<div class="top">
 			<header>
 				<div class="headerUp">
@@ -54,48 +55,50 @@
 					</ul>
 				</nav>
 			</header>
-			<main class="titleMain">
-				<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-				  <!-- Indicators -->
-				  <ol class="carousel-indicators">
-				    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-				    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-				    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-				  </ol>
+			<div class="container">
+				<main class="titleMain">
+					<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+					  <!-- Indicators -->
+					  <ol class="carousel-indicators">
+					    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+					    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+					    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+					  </ol>
 
-				  <!-- Wrapper for slides -->
-				  <div class="carousel-inner" role="listbox">
-				    <div class="item active">
-				      <img class="center-block" src="img/main-theme.jpg" alt="main-theme">
-				      <div class="carousel-caption">
-				        
-				      </div>
-				    </div>
-				    <div class="item">
-				      <img class="center-block" src="img/smallPrice.jpg" alt="smallPrice">
-				      <div class="carousel-caption">
-				        
-				      </div>
-				    </div>
-				    <div class="item">
-				      <img class="center-block" src="img/birthDay.jpg" alt="birthday">
-				      <div class="carousel-caption">
-				        
-				      </div>
-				    </div>
-				  </div>
+					  <!-- Wrapper for slides -->
+					  <div class="carousel-inner" role="listbox">
+					    <div class="item active">
+					      <img class="center-block" src="img/main-theme.jpg" alt="main-theme">
+					      <div class="carousel-caption">
+					        
+					      </div>
+					    </div>
+					    <div class="item">
+					      <img class="center-block" src="img/main-theme.jpg" alt="smallPrice">
+					      <div class="carousel-caption">
+					        
+					      </div>
+					    </div>
+					    <div class="item">
+					      <img class="center-block" src="img/main-theme.jpg" alt="birthday">
+					      <div class="carousel-caption">
+					        
+					      </div>
+					    </div>
+					  </div>
 
-				  <!-- Controls -->
-				  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-				    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-				    <span class="sr-only">Previous</span>
-				  </a>
-				  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-				    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-				    <span class="sr-only">Next</span>
-				  </a>
-				</div>
-			</main>
+					  <!-- Controls -->
+					  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+					    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+					    <span class="sr-only">Previous</span>
+					  </a>
+					  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+					    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+					    <span class="sr-only">Next</span>
+					  </a>
+					</div>
+				</main>
+			</div>	
 		</div>
 		<footer>
 			<div class="copyr">&copy; Все права защищены. 2018г.</div>
