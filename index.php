c6b3125fb9704e7cf3c015d1fc9180de610356d5
Sokol-shko <sokol-shkoldin@mail.ru>
2018-09-29 20:21:54 +0400

    index next

--- index.php
+++ index.php
@@ -8,7 +8,9 @@
 	<link rel="stylesheet" href="libraries/normalize/normalize.css">
 	<link rel="stylesheet" href="libraries/bootstrap-3.3.7-dist/css/bootstrap.min.css">
 	<link rel="stylesheet" href="css/style.css">
-	<link href="https://fonts.googleapis.com/css?family=Fira+Sans:400,700&amp;subset=cyrillic,cyrillic-ext" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:400,700&amp;subset=cyrillic" rel="stylesheet">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" 
+				integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 </head>
 <body>
 	<div class="block">
@@ -20,14 +22,14 @@
 						|
 						<a href="#">Регистрация</a>
 					</div>
-					<div class="box2">
+					<a href="index.php" class="box2">
 						<img src="img/logo.png" alt="logo">
 						<p>AudioCar</p>
-					</div>
+					</a>
 					<div class="box3">
 						<a href="#">Гость</a>
-						<img src="#" alt="Корзина">
-						<p>Цена</p>
+						<a href="#"><img src="img/e-commerce.png" alt="Корзина"></a>
+						<span>0</span>  <i class="fas fa-ruble-sign"></i>
 					</div>
 				</div>
 				<nav>
@@ -57,46 +59,7 @@
 			</header>
 			<div class="container">
 				<main class="titleMain">
-					<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-					  <!-- Indicators -->
-					  <ol class="carousel-indicators">
-					    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-					    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-					    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-					  </ol>
-
-					  <!-- Wrapper for slides -->
-					  <div class="carousel-inner" role="listbox">
-					    <div class="item active">
-					      <img class="center-block" src="img/main-theme.jpg" alt="main-theme">
-					      <div class="carousel-caption">
-					        
-					      </div>
-					    </div>
-					    <div class="item">
-					      <img class="center-block" src="img/main-theme.jpg" alt="smallPrice">
-					      <div class="carousel-caption">
-					        
-					      </div>
-					    </div>
-					    <div class="item">
-					      <img class="center-block" src="img/main-theme.jpg" alt="birthday">
-					      <div class="carousel-caption">
-					        
-					      </div>
-					    </div>
-					  </div>
-
-					  <!-- Controls -->
-					  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-					    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-					    <span class="sr-only">Previous</span>
-					  </a>
-					  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-					    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-					    <span class="sr-only">Next</span>
-					  </a>
-					</div>
+					
 				</main>
 			</div>	
 		</div>
